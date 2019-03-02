176713fcdeafa0d65c54ca7076350ff4e8b9fbd2
Moez2019 <47690135+Moez2019@users.noreply.github.com>
2019-03-02 14:41:30 +0100

    first

---
+++ js/print4.js
@@ -0,0 +1,19 @@
+
+               function pol2(e) {
+                $("#sent2").css("fontSize",e.value);
+          
+               }
+                   function bold(){
+                     $("#sent2").css("fontWeight","bold");
+                   }
+            function italic() {
+              $("#sent2").css("fontStyle","italic");
+            }
+            function souligne() {
+              $("#sent2").css("textDecoration","underline");
+            }
+            
+          function font1(e) {
+            $("#sent2").css("fontFamily",e.value);
+           
+           }
